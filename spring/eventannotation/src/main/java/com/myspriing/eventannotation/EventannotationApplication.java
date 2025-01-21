23db97dc1116bb0a9e3a465fafcdbdf6cec0b50b
박지섭 <bagjiseob@bagjiseob-ui-MacBookPro.local>
2025-01-21 09:55:36 +0900

    [feat] initial commit of spring event listener demo project

---
+++ spring/eventannotation/src/main/java/com/myspriing/eventannotation/EventannotationApplication.java
@@ -0,0 +1,13 @@
+package com.myspriing.eventannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventannotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventannotationApplication.class, args);
+	}
+
+}
---
+++ spring/eventannotation/src/test/java/com/myspriing/eventannotation/EventannotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspriing.eventannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventannotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
